446fdd0234da0977e446a56e210d105bcb3719cf
Amrendra Vimal <vimal.extc@gmail.com>
2017-08-15 20:45:02 -0400

    Initial commit

---
+++ src/main/java/com/bulkprocess/bulkprocess/BulkprocessApplication.java
@@ -0,0 +1,12 @@
+package com.bulkprocess.bulkprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkprocessApplication.class, args);
+	}
+}

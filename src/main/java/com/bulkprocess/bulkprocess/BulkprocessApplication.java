4cbf8938e2140e0ed794dcdd1f3a4c2538ca2f04
Amrendra Vimal <vimal.extc@gmail.com>
2017-08-18 00:26:29 -0400

    Multithreaded

--- src/main/java/com/bulkprocess/bulkprocess/BulkprocessApplication.java
+++ src/main/java/com/bulkprocess/bulkprocess/BulkprocessApplication.java
@@ -2,17 +2,26 @@ package com.bulkprocess.bulkprocess;
 
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BulkprocessApplication {
 
+	public final static Logger logger = LoggerFactory.getLogger(BulkprocessApplication.class);
+
 	public static void main(String[] args) {
 		
 		System.setProperty("inputFile", "file:///" + new File("C:/workspace/bulkprocess/src/main/resources/epsilon.txt").getAbsolutePath());
+
+		long time = System.currentTimeMillis();
 		
 		SpringApplication.run(BulkprocessApplication.class, args);
+
+		time = System.currentTimeMillis() - time;
+		logger.info("Runtime: {} seconds.", ((double)time/1000));
 	}
 
 	/*@Bean
--- src/main/java/com/bulkprocess/bulkprocess/service/BulkProcessing.java
+++ src/main/java/com/bulkprocess/bulkprocess/service/BulkProcessing.java
@@ -5,29 +5,46 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.bulkprocess.bulkprocess.domain.CampaignTO;
 import com.bulkprocess.bulkprocess.step.ChunkReadAndWriteStep;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
 @Configuration
 @EnableBatchProcessing
 public class BulkProcessing {
+
+	@Value("${chunk-size}")
+	private int chunkSize;
+
+	@Value("${max-threads}")
+	private int maxThreads;
 	
 	@Bean
 	Job bulkProcessJob(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, ChunkReadAndWriteStep campaignStep) throws Exception{
 		
 		Step step = stepBuilderFactory.get("file->db")
-				.<CampaignTO, CampaignTO>chunk(1000)
+				.<CampaignTO, CampaignTO>chunk(chunkSize)
 				.reader(campaignStep.fileReader())
 				.writer(campaignStep.jdbcWriter())
+				.taskExecutor(taskExecutor())
 				.build();
 
 		return jobBuilderFactory.get("bulkprocess2")
 				.start(step)
 				.build();
 	}
-	
+
+
+	@Bean
+	public TaskExecutor taskExecutor() {
+		SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
+		taskExecutor.setConcurrencyLimit(maxThreads);
+		return taskExecutor;
+	}
 
 }
